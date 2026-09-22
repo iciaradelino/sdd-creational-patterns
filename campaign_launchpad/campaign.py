72b28117041e7bba3ae071d367b19b14f030e794
iciaradelino <iciaradelinoordax@gmail.com>
2026-09-22 12:01:07 +0200

    exercise 1

--- campaign_launchpad/campaign.py
+++ campaign_launchpad/campaign.py
@@ -18,37 +18,64 @@ class Campaign:
 
 class CampaignBuilder:
     def __init__(self):
-      # TODO
-      pass
+      self._name = None
+      self._channel = None
+      self._daily_budget = None
+      self._start_date = None
+      self._end_date = None
+      self._target_audience: Dict[str, Any] = {}
+      self._creatives: List[Dict[str, str]] = []
+      self._tracking: Dict[str, str] = {}
 
     def with_name(self, name: str):
-      # TODO
-      pass
+      self._name = name
+      return self
 
     def with_channel(self, channel: str):
-      # TODO
-      pass
+      self._channel = channel
+      return self
 
     def with_budget(self, daily_budget: float):
-      # TODO
-      pass
+      self._daily_budget = daily_budget
+      return self
 
     def with_dates(self, start_date, end_date=None):
-      # TODO
-      pass
+      self._start_date = start_date
+      self._end_date = end_date
+      return self
 
     def with_audience(self, **kwargs):
-      # TODO
-      pass
+      self._target_audience = dict(kwargs)
+      return self
 
     def add_creative(self, headline: str, image_url: str):
-      # TODO
-      pass
+      self._creatives.append({"headline": headline, "image_url": image_url})
+      return self
 
     def with_tracking(self, **kwargs):
-      # TODO
-      pass
+      self._tracking = dict(kwargs)
+      return self
 
     def build(self) -> Campaign:
-      # TODO: Validations and return Campaign instance
-      pass
+      if not self._name:
+        raise ValueError("Name is required")
+      if not self._channel:
+        raise ValueError("Channel is required")
+      if self._daily_budget is None or self._daily_budget <= 0:
+        raise ValueError("Daily budget must be positive")
+      if self._start_date is None:
+        raise ValueError("Start date is required")
+      if self._end_date is not None and self._start_date > self._end_date:
+        raise ValueError("Start date must be before or equal to the end date")
+      if not self._creatives:
+        raise ValueError("At least one creative is required")
+      return Campaign(
+        name=self._name,
+        channel=self._channel,
+        daily_budget=self._daily_budget,
+        start_date=self._start_date,
+        end_date=self._end_date,
+        target_audience=self._target_audience,
+        creatives=self._creatives,
+        tracking=self._tracking
+      )
